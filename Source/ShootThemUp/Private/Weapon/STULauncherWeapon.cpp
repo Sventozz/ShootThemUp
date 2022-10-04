489cde0e87e76e87ed80c773f1245ab0d4f01503
Oleksandr <brovarsv@gmail.com>
2022-10-04 23:17:24 +0300

    54. Гранатомёт 1: спаун ракеты

--- Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
@@ -1,5 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Weapon/STULauncherWeapon.h"
+#include "Kismet/GameplayStatics.h"
+#include "Weapon/STUProjectile.h"
 
+void ASTULauncherWeapon::StartFire()
+{
+    MakeShot();
+}
+
+void ASTULauncherWeapon::MakeShot()
+{
+    const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+    auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+    // set projectiles params
+
+    UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+}
--- Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
@@ -4,14 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "Weapon/STUBaseWeapon.h"
+
 #include "STULauncherWeapon.generated.h"
 
-/**
- * 
- */
+class ASTUProjectile;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTULauncherWeapon : public ASTUBaseWeapon
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+  public:
+    virtual void StartFire() override;
+
+  protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+    TSubclassOf<ASTUProjectile> ProjectileClass;
+
+    virtual void MakeShot() override;
 };
